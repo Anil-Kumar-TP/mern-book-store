c753e0fd108bccdfe9cee1cd6a2d222c08af44fc
Anil <anil6996f@gmail.com>
2024-07-27 21:32:32 +0530

    display all the books in the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { BiUserCircle } from 'react-icons/bi';
+import { PiBookOpenTextLight } from 'react-icons/pi'
 
 const Home = () => {
     const [books, setBooks] = useState([]);
@@ -17,7 +19,7 @@ const Home = () => {
                 setBooks(res.data.data);
                 setLoading(false);
             })
-    
+
             .catch((error) => {
                 console.log(error)
                 setLoading(false);
@@ -33,48 +35,33 @@ const Home = () => {
                 </Link>
             </div>
             {loading ? <Spinner /> :
-                <table className='w-full border-separate border-spacing-2'>
-                    <thead>
-                        <tr>
-                            <th className='border border-slate-600 rounded-md'>No</th>
-                            <th className='border border-slate-600 rounded-md'>Title</th>
-                            <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-                            <th className='border border-slate-600 rounded-md max-md:hidden'>Published Year</th>
-                            <th className='border border-slate-600 rounded-md'>Operations</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {books.map((book, index) => {
-                           return <tr key={book._id} className='h-8'>
-                                <td className='border border-slate-700 rounded-md text-center'>
-                                    {index + 1}
-                                </td>
-                                <td className='border border-slate-700 rounded-md text-center'>
-                                    {book.title}
-                                </td>
-                                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                                    {book.author}
-                                </td>
-                                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                                    {book.publishYear}
-                                </td>
-                                <td className='border border-slate-700 rounded-md text-center'>
-                                    <div className='flex justify-center gap-x-4'>
-                                        <Link to={`/books/details/${book._id}`}>
-                                            <BsInfoCircle className='text-2xl text-gray-800' />
-                                        </Link>
-                                        <Link to={`/books/edit/${book._id}`}>
-                                            <AiOutlineEdit className='text-2xl text-yellow-600' />
-                                        </Link>
-                                        <Link to={`/books/delete/${book._id}`}>
-                                            <MdOutlineDelete className='text-2xl text-red-600' />
-                                        </Link>
-                                    </div>
-                                </td>
-                            </tr>
-                        })}
-                    </tbody>
-                </table>
+                <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                    {books.map((book) => {
+                        return <div key={book._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
+                            <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>{book.publishYear}</h2>
+                            <h4 className='my-2 text-gray-500'>{book._id}</h4>
+                            <div className='flex justify-start items-center gap-x-2'>
+                                <PiBookOpenTextLight className='text-red-300 text-2xl' />
+                                <h2 className='my-1'>{book.title}</h2>
+                            </div>
+                            <div className='flex justify-start items-center gap-x-2'>
+                                <BiUserCircle className='text-red-300 text-2xl' />
+                                <h2 className='my-1'>{book.author}</h2>
+                            </div>
+                            <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+                                <Link to={`/books/details/${book._id}`}>
+                                    <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
+                                </Link>
+                                <Link to={`/books/edit/${book._id}`}>
+                                    <AiOutlineEdit className='text-2xl text-yellow-600 hover:text-black' />
+                                </Link>
+                                <Link to={`/books/delete/${book._id}`}>
+                                    <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
+                                </Link>
+                            </div>
+                        </div>
+                    })}
+                </div>
             }
         </div>
     );
