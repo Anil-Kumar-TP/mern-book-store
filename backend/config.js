d63284a5bf41bbe30fd19ff50390e2e7562e2d43
Anil <anil6996f@gmail.com>
2024-07-27 12:27:49 +0530

    connected to DB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+
+export const MONGO_URL = 'mongodb+srv://Anil:NOp89XIrlQiIIK5Y@book-store.mke02oj.mongodb.net/Book-Store?retryWrites=true&w=majority&appName=book-store'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
-import {PORT} from './config.js'
+import { MONGO_URL, PORT } from './config.js'
+import mongoose from 'mongoose';
+
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`)
+app.get('/', (req, res) => {
+    res.status(234).send('welcome!')
 })
+
+
+
+mongoose.connect(MONGO_URL).then(() => {
+    console.log('connected to DB');
+    app.listen(PORT, () => {
+        console.log(`server is listening on port ${PORT}`)
+    });
+}).catch((error) => {
+    console.log('cannot connect to DB')
+});
