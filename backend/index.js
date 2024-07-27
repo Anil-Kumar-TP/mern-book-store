44a30bf827a75bd0821ed9712d9837a33e8606b0
Anil <anil6996f@gmail.com>
2024-07-27 17:30:51 +0530

    fetched all the books from backend and displayed on frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { MONGO_URL, PORT } from './config.js'
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js'
 import cors from 'cors';
 
@@ -9,14 +8,15 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders:['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders:['Content-Type'],
+//     })
+// );
 
 app.use('/books', booksRoute);
 
