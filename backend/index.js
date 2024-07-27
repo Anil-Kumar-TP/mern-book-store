9d9c49dc599c3c00c3bb9be7984a8c0437160bd6
Anil <anil6996f@gmail.com>
2024-07-27 14:07:31 +0530

    created various routes

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import { MONGO_URL, PORT } from './config.js'
 import mongoose from 'mongoose';
-
+import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(234).send('welcome!')
-})
+app.use(express.json());
 
 
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders:['Content-Type'],
+    })
+);
+
+app.use('/books', booksRoute);
 
 mongoose.connect(MONGO_URL).then(() => {
     console.log('connected to DB');
